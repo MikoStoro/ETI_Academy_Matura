85128ff723c6cbd6090ebf72ad594057f267d990
Mikołaj Storoniak <miko.storo@gmail.com>
2024-04-22 22:18:59 +0200

    finished pres2

--- prezentacja2-materialy/ros-mal2.py
+++ prezentacja2-materialy/ros-mal2.py
@@ -1,17 +1,17 @@
-
 def czy_ros_mal(series):
     mal = False
-    if not(series[0] < series[1]):
+    if not (series[0] < series[1]):
         return False
 
-    for i in range(1, len(series)-1):
+    for i in range(1, len(series) - 1):
         if mal:
-            if not(series[i] > series[i+1]):
+            if not (series[i] > series[i + 1]):
                 return False
         else:
-            if not(series[i] < series[i+1]):
+            if not (series[i] < series[i + 1]):
                 mal = True
     return True
+
 
 file = open("informatyka-2023-maj-matura-rozszerzona-zalaczniki/Dane_2305/pi_przyklad.txt")
 
@@ -25,8 +25,8 @@ curr_len = 3
 max_index = -1
 max_len = -1
 
-while(curr_index + curr_len < len(numbers)):
-    slc = numbers[curr_index:curr_index+curr_len]
+while (curr_index + curr_len < len(numbers)):
+    slc = numbers[curr_index:curr_index + curr_len]
     if czy_ros_mal(slc):
         if curr_len > max_len:
             max_index = curr_index
@@ -35,11 +35,6 @@ while(curr_index + curr_len < len(numbers)):
     else:
         curr_index += 1
         curr_len = 3
-print(numbers[max_index:max_index+max_len])
-
-
-
-
-
+print(numbers[max_index:max_index + max_len])
 
 
